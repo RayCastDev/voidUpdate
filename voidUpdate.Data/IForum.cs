4a5cf3c3450470e543a499cdabc38160ce8fdbf2
RayCast <vanyademchenko@mail.ru>
2019-10-24 18:33:42 +0300

    Add service layer

---
+++ voidUpdate.Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using voidUpdate.Data.Models;
+
+namespace voidUpdate.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
